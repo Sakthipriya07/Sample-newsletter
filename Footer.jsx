3b34f969e65665a47fd59c31f1464779fa5e69b7
Sakthipriya07 <166027450+Sakthipriya07@users.noreply.github.com>
2026-04-26 22:03:25 +0530

    Add Footer component with copyright notice

---
+++ Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div>
+      <footer className="footer">
+        <p className="footer-text">
+          © 2024 Newsletter, All rights reserved
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
